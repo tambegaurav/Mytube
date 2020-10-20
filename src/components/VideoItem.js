c600bbbb12b5d198a27ef4b03ad86af6f1ba5325
tambegaurav <gauravkishortambe@gmail.com>
2020-10-20 19:10:11 +0530

    fixes

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,5 +1,5 @@
-import "./VideoItem.css";
 import React from "react";
+import "./VideoItem.css";
 
 const VideoItem = ({ video, onVideoSelect }) => {
   return (
@@ -13,12 +13,15 @@ const VideoItem = ({ video, onVideoSelect }) => {
         border: "3px solid #696969",
         width: "100%",
         borderRadius: "5px",
+        display: "flex",
+        flexDirection: "row",
       }}
     >
       <img
         alt={video.snippet.title}
         className="ui image"
         src={video.snippet.thumbnails.medium.url}
+        width="50%"
       />
       <div className="content">
         <div className="header ui fluid container">{video.snippet.title}</div>
